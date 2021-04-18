5b2ad2f8f507a8854a0c3322954937c4c8bb1712
Luanaap <l.andrade2711@gmail.com>
2021-04-18 20:28:59 -0300

    Initial commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu Primeiro programa: trabalhando com Variáveis");
+
+const idade = 23;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
