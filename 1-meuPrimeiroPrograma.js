f62c5c18d5a69f0f306e988738c9f124262d0f83
Luanaap <l.andrade2711@gmail.com>
2021-04-19 01:51:25 -0300

    JavaScript: primeiros passos com a linguagem

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -10,3 +10,4 @@ console.log(idade*2);
 
 const idadeSomada = idade+2;
 console.log(idadeSomada);
+
---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,12 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+ const idade = 23;
+ const nome = "Luana";
+
+console.log(idade);
+console.log(nome);
+
+//não fazer
+//mes = "Janeiro";
